2b2070dbce227238722935cd462724e45a7b6c22
Akarthikeyan91 <47426252+Akarthikeyan91@users.noreply.github.com>
2019-03-14 16:03:00 +0530

    dict sort

---
+++ dict_sort.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python 
+
+
+dictionary1={"abcd":1436,"vaa":821,"bat":6782}
+print(dict(sorted(dictionary1.items(),key=lambda x:x[0])))
+print(dict(sorted(dictionary1.items(),key=lambda x:x[1])))
+
+
+import operator
+dictionary1={"d":14,"a":23,"b":6,"c":17}
+print(dict(sorted(dictionary1.items(), key=operator.itemgetter(0))))
+print(dict(sorted(dictionary1.items(), key=operator.itemgetter(1))))
