cc83df6c9b9d817d5eb1a392cf4c178c641ef1c6
Akarthikeyan91 <47426252+Akarthikeyan91@users.noreply.github.com>
2019-03-14 15:48:31 +0530

    iter.py

---
+++ iter.py
@@ -0,0 +1,24 @@
+!/usr/bin/python 
+
+class Reverse:
+    """Iterator for looping over a sequence backwards."""
+    def __init__(self, data):
+        self.data = data
+        self.index = len(data)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index == 0:
+            raise StopIteration
+        self.index = self.index - 1
+        return self.data[self.index]
+
+rev=Reverse('hai')
+#print(rev)
+iter(rev)
+for char in rev:
+    print(char)
+
+#print(tuple(rev))
