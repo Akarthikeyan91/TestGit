2653f8b259e99b1f8b05ca01f3e41a0257b375ce
Akarthikeyan91 <47426252+Akarthikeyan91@users.noreply.github.com>
2019-03-14 16:01:01 +0530

    string reverse

---
+++ reverse.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+###### String reverse with range ######
+s = "Hello"
+s1= ""
+n = len(s) + 1
+for i in range(-1,-n,-1):
+    s1 =  s1 +  s[i]
+print(s1)
+
+s = "Hello"             ###### simplest way
+print(''.join(s[i] for i in range(-1,-(len(s)+1),-1)))
+
+print(''.join(reversed('a string')))      ###### sorted
+
+###### String reverse with slice operation #######
+s='python'
+print(s[::-1])
+
+###### String reverse with for loop ##########
+def reverse(text):
+    s=''
+    for i in text:
+        s= i + s
+    return s
+
+print(reverse('hai'))
+    
+
+###### String reverse with recursive method ##########
+def reverse(s): 
+    if len(s) == 0: 
+        return s 
+    else: 
+        return reverse(s[1:]) + s[0]
+  
+s = "Geeksforgeeks"
+  
+print("The original string  is : ") 
+print(s) 
+  
+print("The reversed string(using recursion) is : ") 
+print(reverse(s)) 
+
+##################################################
+# Python code to reverse a string  
+# using reversed() 
+  
+# Function to reverse a string 
+def reverse(string): 
+    string = "".join(reversed(string)) 
+    return string 
+  
+s = "Geeksforgeeks"
+  
+print ("The original string  is : ",) 
+print (s) 
+  
+print ("The reversed string(using reversed) is : ") 
+print (reverse(s))
