24b173ecf0794e9be50df340908b1b20879cbf78
Akarthikeyan91 <47426252+Akarthikeyan91@users.noreply.github.com>
2019-03-14 15:49:05 +0530

    regexp

---
+++ regexp.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import re
+
+line = "dogs Cats are smarter than Cats dogs";
+
+matchObj = re.match( r'.*Cats', line, re.M|re.I)
+if matchObj:
+   print "match --> matchObj.group() : ", matchObj.group()
+else:
+   print "No match!!"
+
+searchObj = re.search( r'.*dogs', line, re.M|re.I)
+if searchObj:
+   print "search --> searchObj.group() : ", searchObj.group()
+   print "search --> searchObj.group() : ", searchObj.span()
+else:
+   print "Nothing found!!"
